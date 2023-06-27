4868d4244d1fd916ffbda263e428d86a8b934412
Chinh Do <chinh@doinby.co>
2023-06-27 10:53:44 +0700

    Add hero styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,28 @@
 export default function Home() {
 	return (
 		<main>
-			<section id='hero' className='relative flex justify-end gradient-bg'>
-				<div className='absolute right-8 w-[75ch]'>
-					<h1 className='text-left text-5xl leading-tight tracking-tight font-normal'>
+			<section id='hero' className='flex justify-center'>
+				<article className='relative flex flex-col justify-centter min-w-fit text-gray-500'>
+					<h2 className='w-fit text-5xl leading-tight tracking-tight'>
 						Giáo dục không phải là sự chuẩn bị <br /> cho cuộc sống — Giáo dục chính
-						là{' '}
-					</h1>
-					{/* <h1 className='self-start ml-28 px-4 text-5xl leading-tight tracking-tight font-bold text-white bg-red-500'>
-						Cuộc Sống
-					</h1> */}
-					<h1 className='w-fit text-left pr-6 pb-1 text-5xl leading-tight tracking-tight font-semibold border-b-4 border-teal-300'>
-						cuộc sống
-					</h1>
-				</div>
-				<div className='prose max-w-none absolute left-8 bottom-12'>
-					<h2 className='font-normal'>
-						Dự án Trường học hạnh phúc <br /> định hướng trở thành tổ chức giáo dục{' '}
-						<br />
-						phi lợi nhuận hỗ trợ các trường xây dựng
-						<br /> ngôi trường hạnh phúc
+						là <br />{' '}
+						<span className='font-semibold text-emerald-400 underline underline-offset-[12px]'>
+							cuộc sống
+						</span>
 					</h2>
-				</div>
-				<img
-					src='/images/sharon-pittaway-iMdsjoiftZo-unsplash.png'
-					alt='colorful-flower'
-					className='mt-16 mr-16 w-[500px] h-[500px] z-10'
-				/>
+					<img
+						src='/images/sharon-pittaway-iMdsjoiftZo-unsplash.png'
+						alt='colorful-flower'
+						className='absolute top-[80px] self-center w-[500px] h-[500px]'
+					/>
+					<p className='w-fit mt-[280px] self-end'>
+						Với sứ mệnh hỗ trợ <br />
+						giáo dục mầm non
+						<br /> xây dựng và nuôi
+						<br /> dưỡng ngôi trường
+						<br /> hạnh phúc
+					</p>
+				</article>
 
 				{/* <p>
 					Considerato che il riconoscimento della dignità inerente a tutti i membri
